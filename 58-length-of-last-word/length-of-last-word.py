6c9d5d748757cedc1d35362db2dc7e7a0003206c
shrmavineet <shrmavineet8@gmail.com>
2026-08-18 14:04:32 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (8.49%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        count = 0
+
+        for i in range(len(s) - 1, -1, -1):
+            if s[i] == " ":
+                if count > 0:
+                    break
+            else:
+                count += 1
+
+        return count
